4ee3b827f8f3d04fcefc68c79f743dada100b911
BOUHASSOUN IDRISS p1004755 <p1004755@univ-lyon1.fr>
2017-04-18 11:36:08 +0200

    Add methods to Ants

--- LangtonsAnts/LangtonsAnts/Ant.cs
+++ LangtonsAnts/LangtonsAnts/Ant.cs
@@ -8,6 +8,34 @@ namespace LangtonsAnts
 {
     public class Ant
     {
+        public int PositionX { get; private set; }
+        public int PositionY { get; private set; }
+
+        public Ant(int positionX, int positionY)
+        {
+            PositionX = positionX;
+            PositionY = positionY;
+        }
+
+        public void MoveAntTop()
+        {
+            
+        }
+
+        public void MoveAntBot()
+        {
+
+        }
+
+        public void MoveAntLeft()
+        {
+
+        }
+
+        public void MoveAntRight()
+        {
+
+        }
 
     }
 }
